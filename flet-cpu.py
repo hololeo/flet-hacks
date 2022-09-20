86c2dfd34241664d4ec848507b4eed857d1792f8
UX Hololeo <helloearth.us@gmail.com>
2022-09-20 14:12:20 -0700

    Flet cpu threads test

---
+++ flet-cpu.py
@@ -0,0 +1,21 @@
+
+import flet
+from flet import Page, Text
+from flet import threading
+import time
+
+def main (page:Page):
+    def on_thread (e):
+        while e.th.running is True:
+            # print ("thread tick")
+            page.txt.value = time.time()
+            page.update()
+            time.sleep (0.1)
+
+    page.th = threading.Thread (target=on_thread, args=[page], daemon= True)
+    page.th.running = True
+    page.th.start()
+    page.txt = Text ()
+    page.add (page.txt)
+    page.update()    
+flet.app (target=main)
