fef5f5d3914d488daff8567ccce815d3245c31a2
UX Hololeo <helloearth.us@gmail.com>
2022-09-21 09:44:03 -0700

    add flet metal spot prices

---
+++ flet-spot.py
@@ -0,0 +1,67 @@
+# Flet Metals Spot Prices by hololeo
+
+import flet
+from flet import Page, Tab, Tabs, Image
+import time
+import threading
+
+isRunning = False
+
+def update_App():
+    global isRunning
+    update_tabs()
+    time.sleep (60)
+    update_App()
+
+def getSilver():
+    url = f"https://www.kitco.com/images/live/silver.gif?ts={time.time()}"
+    return Image(src=url)
+
+def getGold():
+    url = f"https://www.kitco.com/images/live/gold.gif?ts={time.time()}"
+    img = Image(src=url)
+    return img
+
+def getPlatinum():
+    ts = time.clock_gettime_ns(0)
+    url = f"https://www.kitco.com/images/live/plati.gif?ts={time.time()}"
+    return Image(src=url)
+
+def tab_on_change (e):
+    update_tabs ()
+
+def update_tabs ():
+    t = gPage.tabs
+    t.tabs[0].content = getGold()
+    t.tabs[1].content = getSilver()
+    t.tabs[2].content = getPlatinum()
+    t.update()  
+
+def buildTabs(page):
+    t = Tabs(
+        selected_index=1,
+        animation_duration=300,
+        tabs=[
+            Tab (text="Gold",content=getGold(),),
+            Tab (text="Silver",content=getSilver(),),
+            Tab (text="Platinum",content=getPlatinum(),),
+        ],
+        expand=1,
+        on_change = tab_on_change
+    )
+    return t
+
+def main(page: Page):
+    global gPage
+    gPage = page
+    page.title = "Flet Spot - Hololeo Labs"
+    page.tabs = buildTabs(page)
+    page.add(page.tabs)
+    page.update()
+    isRunning = True
+    th = threading.Thread (target=update_App, args={}, daemon=True)
+    th.start()
+    
+
+flet.app(target=main)
+
