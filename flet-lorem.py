f5bd212bae1d91a50e3573d2b5e30accd15eba98
UX Hololeo <helloearth.us@gmail.com>
2022-09-21 19:41:31 -0700

    add header text, output textfield

--- flet-lorem.py
+++ flet-lorem.py
@@ -1,6 +1,6 @@
 # Flet Lorem Ipsum Generator - Hololeo Labs
 import flet
-from flet import Page, Row, Text, TextField, Slider, Dropdown, dropdown, padding
+from flet import Page, Row, Column, Text, TextField, Slider, Dropdown, dropdown, padding
 
 def main(page: Page):
     page.title = "Flet Lorem Ipsum Generator - Hololeo Labs"
@@ -9,7 +9,28 @@ def main(page: Page):
     page.bgcolor = "#262d2f"
     page.vertical_alignment = "start"
     page.padding = 10
+    page.header = Text (
+        value = "Flet Lorem Ipsum Generator",
+        size = 25
+    )
+    page.output = TextField (
+        label="",
+        keyboard_type = "number",
+        value = "Sit et, elit facilisi elit tempus fermentum ipsum ipsum ipsum maecenas nec at enim arcu molestie nibh."*1000,
+        content_padding = padding.only (right=20),
+        border_width = 2,
+        max_lines=100,
+        min_lines=10,
+        expand = True,
+        read_only = True 
+    )    
+    page.add (Column ([page.header, page.output], expand= True))
     page.update()
 
 flet.app(target=main, port=7777)
 #flet.app(target=main, port=7777, view=flet.WEB_BROWSER)
+
+
+
+
+
