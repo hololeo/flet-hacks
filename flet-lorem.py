42fe02411caef380b24f963e8b5cf36701c18807
UX Hololeo <helloearth.us@gmail.com>
2022-09-21 20:00:10 -0700

    add ui row number tf , dropdown menu

--- flet-lorem.py
+++ flet-lorem.py
@@ -9,10 +9,54 @@ def main(page: Page):
     page.bgcolor = "#262d2f"
     page.vertical_alignment = "start"
     page.padding = 10
+
+    def update_lorem ():
+        #s = lorem.createText (dd.value,int(tf.value))
+        s = "asdf"
+        t2.value = s
+        page.update()
+
+    def dropdown_changed(e):
+        update_lorem()
+
+    def tf_on_changed (e):
+        try: 
+            val = int(e.control.value)
+            if val > 99:
+                val = 99
+            tf.value = val
+            update_lorem()
+        except:
+            print ("num textfield not number")
+            tf.value = 3
+
+    tf = TextField (
+        label="",
+        keyboard_type = "number",
+        value = 3,
+        max_lines=1,
+        min_lines=1,
+        width=50,
+        on_submit = tf_on_changed,
+        on_blur = tf_on_changed       
+    )
+
+    dd = Dropdown(
+        on_change=dropdown_changed,
+        value = "Paragraph",
+        options=[
+            dropdown.Option("Word"),
+            dropdown.Option("Sentence"),
+            dropdown.Option("Paragraph"),
+        ],
+        width=200,
+    )
+
     page.header = Text (
         value = "Flet Lorem Ipsum Generator",
         size = 25
     )
+    page.ui_row = Row ([tf,dd])
     page.output = TextField (
         label="",
         keyboard_type = "number",
@@ -24,7 +68,7 @@ def main(page: Page):
         expand = True,
         read_only = True 
     )    
-    page.add (Column ([page.header, page.output], expand= True))
+    page.add (Column ([page.header, page.ui_row, page.output], expand= True))
     page.update()
 
 flet.app(target=main, port=7777)
